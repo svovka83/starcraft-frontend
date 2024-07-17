65922fe50937583dd7da290b9a6d0785492f0d87
Volodymyr Smoliak <svovka83@gmail.com>
2024-07-17 19:28:06 +0300

    game

--- src/App.js
+++ src/App.js
@@ -2,6 +2,7 @@ import { Routes, Route } from "react-router-dom";
 
 import Selection from "./pages/Selection/Selection";
 import Start from "./pages/Start/Start";
+import Game from "./pages/Game/Game";
 
 function App() {
   return (
@@ -9,6 +10,7 @@ function App() {
       <Routes>
         <Route path="/" element={<Start />} />
         <Route path="/selection" element={<Selection />} />
+        <Route path="/game" element={<Game />} />
       </Routes>
     </div>
   );
---
+++ src/pages/Game/Game.jsx
@@ -0,0 +1,7 @@
+import styles from "./Game.module.css";
+
+const Game = () => {
+  return <div className={styles.game}>Game</div>;
+};
+
+export default Game;
